da73b34de1d7cc6287821c79961174128e62266e
Shivakumar Gourimath <152435774+shivakumargourimath@users.noreply.github.com>
2025-07-20 22:13:35 +0530

    Update Remove_Duplicates_from_Sorted_Array.py

--- python/array/Remove_Duplicates_from_Sorted_Array.py
+++ python/array/Remove_Duplicates_from_Sorted_Array.py
@@ -7,14 +7,6 @@ class Solution:
                 l+=1
         return l
 
-#for local compiler
-def removeDuplicates(nums):
-        l=1
-        for r in range(1,len(nums)):
-            if nums[r]!=nums[r-1]:
-                nums[l]=nums[r]
-                l+=1
-        return l
-
-result=removeDuplicates([1,1,2,3,4,5,5])
+s=Solution()
+result=s.removeDuplicates([1,1,2,3,4,5,5])
 print(result)
